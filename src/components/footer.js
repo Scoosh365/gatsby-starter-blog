a39584dd5ffc730d0bb2bec3dc71faa1e6d3323b
Scoosh365 <67313846+Scoosh365@users.noreply.github.com>
2024-10-21 11:39:19 +0100

    Create footer.js
    Scoosh added footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <p>&copy; {new Date().getFullYear()} My Gatsby Site</p>
+    <p>Powered by Test</p>
+  </footer>
+);
+
+export default Footer;
